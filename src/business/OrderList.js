2df636808f9b525dcfd74c212def0c86496a0608
Frank Rügamer <frankruegamer@gmail.com>
2019-06-24 18:02:40 +0200

    Use top10 query

--- src/business/OrderList.js
+++ src/business/OrderList.js
@@ -11,27 +11,22 @@ export default class OrderList extends React.Component {
     }
 
     static toOption(order) {
-        let timeStampString = order.timestamp.toISOString();
+        const timeStampString = order.timestamp.toISOString();
         return {key: timeStampString, text: order.timestamp.toLocaleString(), value: timeStampString}
     }
 
     componentDidMount() {
-        fetch("http://localhost:8080/api/orders")
+        fetch("http://localhost:8080/api/orders/search/last10")
             .then(r => r.json())
             .then(data => data._embedded.orders)
             .then(this.addInitialOrders);
-        // fetch("http://localhost:8080/api/orders/search/latest")
-        //     .then(r => r.json())
-        //     .then(order => OrderList.toOption(order))
-        //     .then(option => this.setState({current: option.value}));
     }
 
     addInitialOrders(orders) {
         orders = orders.map(order => {
             order.timestamp = new Date(order.timestamp);
             return order
-        }).sort(order => order.timestamp);
-        console.log(orders);
+        });
         this.setState({
             orders: orders,
             current: OrderList.toOption(orders[0]).value
@@ -45,7 +40,7 @@ export default class OrderList extends React.Component {
     render() {
         const {orders, current} = this.state;
         const options = orders.map(OrderList.toOption);
-        return <Dropdown scrolling options={options} value={current} onChange={this.onItemChange}/>
+        return <Dropdown options={options} value={current} onChange={this.onItemChange}/>
     }
 
 }
