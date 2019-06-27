103d166bc36f054e0bfe9f810fbad16317cfd1f0
Frank <frankruegamer@gmail.com>
2019-06-27 20:21:06 +0200

    Add pagination to food table

--- src/business/FoodList.js
+++ src/business/FoodList.js
@@ -1,26 +1,44 @@
 import React from "react";
-import {Table} from "semantic-ui-react";
+import {Pagination, Table} from "semantic-ui-react";
+import FoodService from "../service/FoodService";
 
 export default class FoodList extends React.Component {
 
     constructor(props, context) {
         super(props, context);
         this.state = {
-            foods: []
+            foods: [],
+            activePage: 1,
+            totalPages: 5
         };
+        this.handlePageChange = this.handlePageChange.bind(this);
+        this.updateFoodList = this.updateFoodList.bind(this);
     }
 
     componentDidMount() {
-        fetch("http://localhost:8080/api/restaurants/1/foods")
-            .then(response => response.json())
-            .then(data => data._embedded.foods)
-            .then(foods => this.setState({
-                foods: foods
-            }));
+        this.updateFoodList(this.state.activePage);
+    }
+
+    handlePageChange(e, {activePage}) {
+        this.setState({
+            activePage: activePage
+        });
+        this.updateFoodList(activePage);
+    }
+
+    updateFoodList(page) {
+        FoodService.getAll(page)
+            .then(json => {
+                this.setState({
+                    foods: json._embedded.foods,
+                    totalPages: json.page.totalPages
+                })
+            });
     }
 
     render() {
-        const rows = this.state.foods.map(food =>
+        const {foods, totalPages, activePage} = this.state;
+        const rows = foods.map(food =>
             <Table.Row key={food.name + food.description}>
                 <Table.Cell>{food.number}</Table.Cell>
                 <Table.Cell>{food.name}</Table.Cell>
@@ -29,19 +47,22 @@ export default class FoodList extends React.Component {
             </Table.Row>
         );
         return (
-            <Table celled>
-                <Table.Header>
-                    <Table.Row>
-                        <Table.HeaderCell>Number</Table.HeaderCell>
-                        <Table.HeaderCell>Name</Table.HeaderCell>
-                        <Table.HeaderCell>Description</Table.HeaderCell>
-                        <Table.HeaderCell>Price</Table.HeaderCell>
-                    </Table.Row>
-                </Table.Header>
-                <Table.Body>
-                    {rows}
-                </Table.Body>
-            </Table>
+            <>
+                <Pagination activePage={activePage} totalPages={totalPages} onPageChange={this.handlePageChange}/>
+                <Table celled>
+                    <Table.Header>
+                        <Table.Row>
+                            <Table.HeaderCell>Number</Table.HeaderCell>
+                            <Table.HeaderCell>Name</Table.HeaderCell>
+                            <Table.HeaderCell>Description</Table.HeaderCell>
+                            <Table.HeaderCell>Price</Table.HeaderCell>
+                        </Table.Row>
+                    </Table.Header>
+                    <Table.Body>
+                        {rows}
+                    </Table.Body>
+                </Table>
+            </>
         );
     }
 }
