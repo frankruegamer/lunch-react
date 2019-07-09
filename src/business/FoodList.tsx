7e8cb37e195234332b5690b80ff8c2cda99426a1
Frank <frankruegamer@gmail.com>
2019-07-09 09:48:10 +0200

    Add food search

--- src/business/FoodList.tsx
+++ src/business/FoodList.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Input, InputOnChangeData, Pagination, PaginationProps} from "semantic-ui-react";
+import {Input, Pagination, PaginationProps, Search, SearchProps} from "semantic-ui-react";
 import FoodService from "../service/FoodService";
 import FoodTable from "./FoodTable";
 import Food from "../domain/Food";
@@ -28,6 +28,7 @@ export default class FoodList extends React.Component<FoodListProps, FoodListSta
         };
         this.handlePageChange = this.handlePageChange.bind(this);
         this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleSearchChange = this.handleSearchChange.bind(this);
         this.updateFoodList = this.updateFoodList.bind(this);
     }
 
@@ -47,7 +48,14 @@ export default class FoodList extends React.Component<FoodListProps, FoodListSta
         this.updateFoodList(this.state.input, activePage as number);
     }
 
-    handleInputChange(event: any, {value}: InputOnChangeData): void {
+    handleInputChange(event: any, {value}: { value: string }): void {
+        this.updateFoodList(value, 1);
+    }
+
+    handleSearchChange(event: any, {value}: SearchProps): void {
+        if (value === undefined) {
+            return;
+        }
         this.updateFoodList(value, 1);
     }
 
@@ -72,9 +80,16 @@ export default class FoodList extends React.Component<FoodListProps, FoodListSta
 
     render(): React.ReactNode {
         const {input, foods, totalPages, activePage} = this.state;
+        const results = foods.map(food => ({
+            key: food.name + food.description,
+            title: food.name,
+            price: food.price.toFixed(2) + " €",
+            description: food.description
+        }));
         return (
             <>
                 <Input placeholder="Search food..." value={input} onChange={this.handleInputChange}/>
+                <Search value={input} onSearchChange={this.handleSearchChange} results={results}/>
                 <Pagination activePage={activePage} totalPages={totalPages} onPageChange={this.handlePageChange}/>
                 <FoodTable foods={foods}/>
             </>
