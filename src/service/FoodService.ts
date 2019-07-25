544a2dc9d727187098f751a9db222598e83dfea5
Frank <frankruegamer@gmail.com>
2019-07-25 16:56:29 +0200

    Cleanup unused service methods

--- src/service/FoodService.ts
+++ src/service/FoodService.ts
@@ -3,27 +3,7 @@ import Hal from "../domain/hal/Hal";
 import Restaurant from "../domain/Restaurant";
 import BackendService from "./BackendService";
 
-function getAll(): Promise<Food[]> {
-    return BackendService.getCollection<Food>("foods");
-}
-
-function getAllPaginated(page: number): Promise<Hal<Food>> {
-    const params = {
-        page: page - 1,
-        size: 10
-    };
-    return BackendService.getPaginatedCollection<Food>("foods", params);
-}
-
 export default class FoodService {
-
-    static getAll(page?: number): Promise<Hal<Food>> | Promise<Food[]> {
-        if (page !== undefined) {
-            return getAllPaginated(page);
-        } else {
-            return getAll();
-        }
-    }
 
     static getFromRegex(regex: string, page: number, restaurant?: Restaurant): Promise<Hal<Food>> {
         const params: { [key: string]: any } = {
