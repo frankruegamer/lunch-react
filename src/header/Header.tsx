e49f4a3015212b867c04bb6e6ab7ec5feed93c11
Frank <frankruegamer@gmail.com>
2019-07-27 01:32:47 +0200

    Move logic to get last 10 orders to Header to control that they only get refetched when adding a new order

--- src/header/Header.tsx
+++ src/header/Header.tsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {Image, Menu, Responsive} from "semantic-ui-react";
 import Order from "../domain/Order";
 import Person from "../domain/Person";
 import Restaurant from "../domain/Restaurant";
 import logo from "../logo.svg";
+import OrderService from "../service/OrderService";
 import LogoutMenuItem from "./LogoutMenuItem";
 import NewOrderMenuItem from "./NewOrderMenuItem";
 import OrderMenuItem from "./OrderMenuItem";
@@ -19,15 +20,32 @@ interface HeaderProps {
     onLogout: () => void;
 }
 
-const Header: React.FC<HeaderProps> = (props) => {
+const Header: React.FC<HeaderProps> = ({onLogout, onOrderChange, onPersonChange, order, person, restaurant}) => {
+    const [orders, setOrders] = useState<Order[]>([]);
+
+    useEffect(() => {
+        OrderService.getLast10()
+            .then(o => {
+                setOrders(o);
+                if (o.length > 0) {
+                    onOrderChange(o[0]);
+                }
+            });
+    }, [onOrderChange]);
+
+    function handleNewOrder(o: Order) {
+        OrderService.getLast10().then(setOrders);
+        onOrderChange(o);
+    }
+
     let rightMenu;
-    if (props.person === undefined) {
-        rightMenu = <PersonMenuItem onPersonChange={props.onPersonChange}/>;
+    if (person === undefined) {
+        rightMenu = <PersonMenuItem onPersonChange={onPersonChange}/>;
     } else {
         rightMenu = (
             <>
-                <Menu.Item content={props.person.name}/>
-                <LogoutMenuItem onLogout={props.onLogout}/>
+                <Menu.Item content={person.name}/>
+                <LogoutMenuItem onLogout={onLogout}/>
             </>
         );
     }
@@ -40,9 +58,9 @@ const Header: React.FC<HeaderProps> = (props) => {
                     Lunch Organizer
                 </Menu.Item>
             </Responsive>
-            <OrderMenuItem order={props.order} onOrderChange={props.onOrderChange}/>
-            <RestaurantMenuItem restaurant={props.restaurant}/>
-            <NewOrderMenuItem onNewOrder={props.onOrderChange}/>
+            <OrderMenuItem order={order} orders={orders} onOrderChange={onOrderChange}/>
+            <RestaurantMenuItem restaurant={restaurant}/>
+            <NewOrderMenuItem onNewOrder={handleNewOrder}/>
             <Menu.Menu position="right">
                 {rightMenu}
             </Menu.Menu>
--- src/header/OrderMenuItem.tsx
+++ src/header/OrderMenuItem.tsx
@@ -1,29 +1,14 @@
-import React, {useEffect, useRef, useState} from "react";
+import React from "react";
 import OrderList from "../business/OrderList";
 import Order from "../domain/Order";
-import OrderService from "../service/OrderService";
 
 interface OrderMenuItemProps {
     order?: Order;
+    orders: Order[];
     onOrderChange: (order: Order) => void;
 }
 
-const OrderMenuItem: React.FC<OrderMenuItemProps> = ({order, onOrderChange}) => {
-    const [orders, setOrders] = useState<Order[]>([]);
-
-    const firstRender = useRef(true);
-
-    useEffect(() => {
-        OrderService.getLast10()
-            .then(o => {
-                setOrders(o);
-                if (firstRender.current && o.length > 0) {
-                    firstRender.current = false;
-                    onOrderChange(o[0]);
-                }
-            });
-    }, [order, onOrderChange]);
-
+const OrderMenuItem: React.FC<OrderMenuItemProps> = ({order, orders, onOrderChange}) => {
     function handleItemChange(timestamp: string): void {
         const current = orders.find(o => o.timestamp.toISOString() === timestamp);
         onOrderChange(current as Order);
