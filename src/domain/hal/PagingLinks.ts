bd072cd298ee955f92ceec37f2e96806a2c274b4
Frank <frankruegamer@gmail.com>
2019-07-28 15:55:29 +0200

    Remove index signature of child interface

--- src/domain/hal/PagingLinks.ts
+++ src/domain/hal/PagingLinks.ts
@@ -2,7 +2,6 @@ import Link from "./Link";
 import Links from "./Links";
 
 export default interface PagingLinks extends Links {
-    [key: string]: Link | undefined;
     first?: Link;
     prev?: Link;
     next?: Link;
