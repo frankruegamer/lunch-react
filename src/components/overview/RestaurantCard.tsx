23a985ec09d7cf754f7453730b5b83fc71696c38
Frank <frankruegamer@gmail.com>
2019-07-29 21:42:24 +0200

    Make restaurant card sticky

--- src/components/overview/RestaurantCard.tsx
+++ src/components/overview/RestaurantCard.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Card, Icon} from "semantic-ui-react";
+import {Card, Icon, Sticky} from "semantic-ui-react";
 import Restaurant from "../../domain/Restaurant";
 
 interface RestaurantCardProps {
@@ -10,18 +10,20 @@ const RestaurantCard: React.FC<RestaurantCardProps> = ({restaurant}) => {
     const {name, address} = restaurant;
     const phone = "+" + restaurant.telephone.replace(/-/g, " ");
     return (
-        <Card>
-            <Card.Content>
-                <Card.Header>{name}</Card.Header>
-                <Card.Description>
-                    <div>{address.street + " " + address.houseNumber}</div>
-                    <div>{address.postalCode + " " + address.city}</div>
-                </Card.Description>
-            </Card.Content>
-            <Card.Content>
-                <div><Icon name="phone"/>{phone}</div>
-            </Card.Content>
-        </Card>
+        <Sticky offset={55}>
+            <Card>
+                <Card.Content>
+                    <Card.Header>{name}</Card.Header>
+                    <Card.Description>
+                        <div>{address.street + " " + address.houseNumber}</div>
+                        <div>{address.postalCode + " " + address.city}</div>
+                    </Card.Description>
+                </Card.Content>
+                <Card.Content>
+                    <div><Icon name="phone"/>{phone}</div>
+                </Card.Content>
+            </Card>
+        </Sticky>
     );
 };
 
